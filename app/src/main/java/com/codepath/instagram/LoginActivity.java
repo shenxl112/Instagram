b5327f9f99fec2c207da9a39d6db22b902570051
“shenxl112” <shenxl1102@gmail.com>
2019-03-25 23:15:02 -0400

    Updated

--- app/src/main/java/com/codepath/instagram/LoginActivity.java
+++ app/src/main/java/com/codepath/instagram/LoginActivity.java
@@ -15,33 +15,57 @@ import com.parse.ParseUser;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
+
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    //private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        etUsername = findViewById(R.id.etUsername);
-        etPassword = findViewById(R.id.etPassword);
-        btnLogin = findViewById(R.id.btnLogin);
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            goMainActivity();
+        } else{
+            //show the login screen or signup
+            etUsername = findViewById(R.id.etUsername);
+            etPassword = findViewById(R.id.etPassword);
+            btnLogin = findViewById(R.id.btnLogin);
+            //btnSignUp = findViewById(R.id.btnSignUp);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username = etUsername.getText().toString();
-                String password = etPassword.getText().toString();
-                login(username, password);
-            }
-        });
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String username = etUsername.getText().toString();
+                    String password = etPassword.getText().toString();
+                    login(username, password);
+                }
+            });
+
+//            btnSignUp.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    // send to SignupActivity
+//                    Log.d(TAG, "Navigating to Signup");
+//                    Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+//                    startActivity(i);
+//                    finish();   // finishes LoginActivity, takes it off the stack
+//                    // when you press back, now exits app
+//                    Log.d(TAG, "finished");
+//                }
+//            });
+        }
     }
+
 
     private void login(String username, String password) {
         ParseUser.logInInBackground(username, password, new LogInCallback() {
             @Override
             public void done(ParseUser user, ParseException e) {
+
                 if(user != null){
                     //TODO: better error handling
                     Log.e(TAG, "Issue with login");
@@ -59,5 +83,6 @@ public class LoginActivity extends AppCompatActivity {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
+        Log.d(TAG, "finished");
     }
 }
--- app/src/main/java/com/codepath/instagram/ParseApplication.java
+++ app/src/main/java/com/codepath/instagram/ParseApplication.java
@@ -3,12 +3,15 @@ package com.codepath.instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         // set applicationId, and server server based on the values in the Heroku settings.
         // clientKey is not needed unless explicitly configured
